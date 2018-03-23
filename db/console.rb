ad4223df3fe280e5334c6ca7924d5a744a9c1f3a
Mike-Heneghan <m.heneghan@live.co.uk>
2018-03-23 14:16:56 +0000

    Film class created with basic CRUD methods.

--- db/console.rb
+++ db/console.rb
@@ -5,11 +5,21 @@ require_relative("../models/film")
 require_relative("../models/ticket")
 
 Customer.delete_all()
+Film.delete_all()
 
 customer1 = Customer.new('name'=>'Mike', 'funds'=>'30')
-
 customer1.save()
 customer1.funds = '45'
 customer1.update()
 # customer1.delete()
+
+film1 = Film.new('title'=>'Memento', 'price'=>'12')
+film1.save()
+film1.price = '11'
+film1.update()
+
 p Customer.all()
+puts ""
+
+p Film.all()
+puts ""
---
+++ models/film.rb
@@ -0,0 +1,70 @@
+require("pry-byebug")
+
+require_relative("customer.rb")
+require_relative("sql_runner.rb")
+require_relative("ticket.rb")
+
+class Film
+  attr_reader :id
+  attr_accessor :title, :price
+
+  def initialize( options )
+    @id = options['id']
+    @title = options['title']
+    @price = options['price']
+  end
+
+  def save()
+    sql = "
+    INSERT INTO films (title, price)
+    VALUES ($1, $2)
+    RETURNING id"
+
+    values = [@title, @price]
+
+    result = SqlRunner.run(sql, values)
+    @id = result[0]['id']
+
+  end
+
+  def self.get_film_objects(result)
+    answer = result.map { |film_hash| Film.new(film_hash)}
+    return answer
+  end
+
+  def self.all()
+    sql = "
+    SELECT * FROM films"
+
+    result = SqlRunner.run(sql)
+    return Film.get_film_objects(result)
+  end
+
+  def self.delete_all()
+    sql = "
+    DELETE FROM films"
+
+    SqlRunner.run(sql)
+  end
+
+  def update()
+    sql = "
+    UPDATE films
+    SET (title, price) = ($1,$2)
+    WHERE id = $3"
+
+    values = [@title, @price, @id]
+
+    result = SqlRunner.run(sql,values)
+  end
+
+  def delete()
+    sql = "
+    DELETE FROM films WHERE id = $1"
+
+    values = [@id]
+
+    result = SqlRunner.run(sql,values)
+  end
+
+end

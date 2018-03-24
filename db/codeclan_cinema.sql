4c1469a5ca52e08aa8096fc089fc89dc7e3718cc
Mike-Heneghan <m.heneghan@live.co.uk>
2018-03-24 17:07:05 +0000

    Screenings table added to codeclan_cinema database.

--- db/codeclan_cinema.sql
+++ db/codeclan_cinema.sql
@@ -1,3 +1,4 @@
+DROP TABLE screenings;
 DROP TABLE tickets;
 DROP TABLE films;
 DROP TABLE customers;
@@ -17,5 +18,11 @@ CREATE TABLE customers (
 CREATE TABLE tickets (
   id SERIAL4 PRIMARY KEY,
   customer_id INT4 REFERENCES customers(id) ON DELETE CASCADE,
+  film_id INT4 REFERENCES films(id) ON DELETE CASCADE
+);
+
+CREATE TABLE screenings (
+  id SERIAL4 PRIMARY KEY,
+  showing VARCHAR(255),
   film_id INT4 REFERENCES films(id) ON DELETE CASCADE
 );
